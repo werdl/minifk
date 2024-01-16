8d947913cc2a173262497d12a9ee4c149076965f
werdl <werdl_is_cool@outlook.com>
2024-01-16 19:18:56 +0000

    small improvement

--- obf.c
+++ obf.c
@@ -2,4 +2,4 @@
 #define c case
 #define s a[1]
 main(int l,char**a){
-int t[30000]={0};int*p=t;while(*s){switch(*s){c'>':++p;b;c'<':--p;b;c'+':++*p;b;c'-':--*p;b;c'.':printf("%c",*p);b;c',':*p=getchar();b;c'[':if(!*p){l=1;while(l>0){s++;if(*s==']'){l--;}else if(*s=='['){l++;}}}b;c']':if(*p){l=1;while(l>0){s--;if(*s=='['){l--;}else if(*s==']'){l++;}}}b;}s++;}}
+int t[30000]={0},*p=t;while(*s){switch(*s){c'>':++p;b;c'<':--p;b;c'+':++*p;b;c'-':--*p;b;c'.':putchar(*p);b;c',':*p=getchar();b;c'[':if(!*p){l=1;while(l>0){s++;if(*s==']'){l--;}else if(*s=='['){l++;}}}b;c']':if(*p){l=1;while(l>0){s--;if(*s=='['){l--;}else if(*s==']'){l++;}}}b;}s++;}}
